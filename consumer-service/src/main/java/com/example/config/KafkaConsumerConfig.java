b45fea7456f0bb5fdb28ecb3e01e80f110e06481
Dhruv Maheshwari <mrdm026@gmail.com>
2023-03-15 11:24:27 +0530

    Added consumer configs file

---
+++ consumer-service/src/main/java/com/example/config/KafkaConsumerConfig.java
@@ -0,0 +1,31 @@
+package com.example.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Autowired
+    private ConsumerFactory<Integer, String> consumerFactory;
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(consumerConfig()));
+        return factory;
+    }
+
+    private Map<String, Object> consumerConfig() {
+        return new HashMap<>(consumerFactory.getConfigurationProperties());
+    }
+}
